ad3cfaec0c72a75db1c7cbd10de19890d649efd5
Martin Dutra <martindsq@hotmail.com>
2020-02-26 17:31:39 -0300

    Fix NetworkKeyTests

--- UnitTests/NetworkKeyTests.swift
+++ UnitTests/NetworkKeyTests.swift
@@ -19,14 +19,9 @@ class NetworkKeyTests: XCTestCase {
     func test_valid() {
 
         // SSB default
-        var key = NetworkKey(base64: "1KHLiKZvAvjbY1ziZEHMXawbCEIM6qwjCDm3VYRan/s=")
+        let key = NetworkKey(base64: "1KHLiKZvAvjbY1ziZEHMXawbCEIM6qwjCDm3VYRan/s=")
         XCTAssertNotNil(key)
         XCTAssertTrue(key == NetworkKey.ssb)
-
-        // Verse development
-        key = NetworkKey(base64: "VersVersVmVyc2UgQ29tbXVuaWNhdGlvbnMsIEluYy4=")
-        XCTAssertNotNil(key)
-        XCTAssertTrue(key == NetworkKey.verse)
     }
 
     func test_invalid() {
@@ -35,7 +30,7 @@ class NetworkKeyTests: XCTestCase {
         var key = NetworkKey(base64: "KHLiKZvvjbY1ziEHMXawbCEIM6qwjCDm3VYRan/")
         XCTAssertNil(key)
         
-        // to short (needs to be 32bytes)
+        // too short (needs to be 32bytes)
         key = NetworkKey(base64: "VmVyc2UgQ29tbXVuaWNhdGlvbnMsIEluYy4=")
         XCTAssertNil(key)
     }
