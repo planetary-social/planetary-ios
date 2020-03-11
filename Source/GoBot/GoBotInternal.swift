3e4b99872254377f6e5a7fc50054a4485128df76
Henry <111202+cryptix@users.noreply.github.com>
2020-03-11 17:10:23 +0100

    New pubs list (#16)
    This cleans up the list of planetary pubs. They all use dedicated domain names now. There were some inconsistencies in the ports, too.

--- Source/GoBot/GoBotInternal.swift
+++ Source/GoBot/GoBotInternal.swift
@@ -95,20 +95,16 @@ class GoBotInternal {
         NetworkKey.ssb.string: ( Peer(tcpAddr: "main2.planetary.social:8008", pubKey: Identities.ssb.pubs["planetary-pub2"]!) , [
             Peer(tcpAddr: "main1.planetary.social:8008", pubKey: Identities.ssb.pubs["planetary-pub1"]!),
             Peer(tcpAddr: "main3.planetary.social:8008", pubKey: Identities.ssb.pubs["planetary-pub3"]!),
-            Peer(tcpAddr: "pub4.planetary.social:8008", pubKey: Identities.ssb.pubs["planetary-pub4"]!)
+            Peer(tcpAddr: "main4.planetary.social:8008", pubKey: Identities.ssb.pubs["planetary-pub4"]!)
         ]),
 
-        NetworkKey.verse.string: (Peer(tcpAddr: "pub1.planetary.social:8118", pubKey: Identities.verse.pubs["testpub_go"]!), [
-            Peer(tcpAddr: "pub1.planetary.social:8008", pubKey: Identities.verse.pubs["testpub_js"]!)
-        ]),
+        NetworkKey.planetary.string: (Peer(tcpAddr: "demo2.planetary.social:7227", pubKey: Identities.planetary.pubs["testpub_go2"]!), [
+            Peer(tcpAddr: "demo1.planetary.social:8008", pubKey: Identities.planetary.pubs["testpub_go1"]!),
+            Peer(tcpAddr: "demo3.planetary.social:8008", pubKey: Identities.planetary.pubs["testpub_go3"]!),
+            Peer(tcpAddr: "demo4.planetary.social:8008", pubKey: Identities.planetary.pubs["testpub_go4"]!),
 
-        NetworkKey.planetary.string: (Peer(tcpAddr: "pub1.planetary.social:7227", pubKey: Identities.planetary.pubs["testpub_go2"]!), [
-            Peer(tcpAddr: "pub1.planetary.social:7117", pubKey: Identities.planetary.pubs["testpub_go1"]!),
-            Peer(tcpAddr: "pub3.planetary.social:7337", pubKey: Identities.planetary.pubs["testpub_go3"]!),
-            Peer(tcpAddr: "demo4.planetary.social:7447", pubKey: Identities.planetary.pubs["testpub_go4"]!),
-            
-            Peer(tcpAddr: "pub2.planetary.social:7557", pubKey: Identities.planetary.pubs["testpub_go_testing1"]!),
-            Peer(tcpAddr: "demo6.planetary.social:7667", pubKey: Identities.planetary.pubs["testpub_go_testing2"]!)
+            Peer(tcpAddr: "demo5.planetary.social:8008", pubKey: Identities.planetary.pubs["testpub_go_testing1"]!),
+            Peer(tcpAddr: "demo6.planetary.social:8008", pubKey: Identities.planetary.pubs["testpub_go_testing2"]!)
         ]),
 
         NetworkKey.integrationTests.string: (Peer(tcpAddr: "testing-ci.planetary.social:9119", pubKey: Identities.testNet.pubs["integrationpub1"]!), [])
