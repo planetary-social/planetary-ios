05fb91b2c92d77d9496bb8a14f8e10ca3c7c6f98
Henry <cryptix@riseup.net>
2020-03-13 14:11:27 +0100

    hide generated text gallery in our posts

--- Source/Model/Post+Blob.swift
+++ Source/Model/Post+Blob.swift
@@ -34,11 +34,12 @@ extension Post {
         if blobs.count > 0 {
             // This is meant as compat with other ssb-clients like Patchwork, etc.
             // TODO: use this marker to cut when viewing in planetary
-            textWithImages += "\n~=~=~Planetary Attachments~=~=~\n"
-            for b in blobs {
-                textWithImages += "![\(b.name)](\(b.identifier))\n"
+            textWithImages += PlanetaryCompatBlobsGallery
+            for (i,b) in blobs.enumerated() {
+                // TODO: captionize!
+                textWithImages += "![no.\(i)](\(b.identifier))\n"
             }
-            textWithImages += "\n~=~=~Planetary Attachments~=~=~\n"
+            textWithImages += PlanetaryCompatBlobsGallery
         }
 
         return Post(blobs: blobs,
@@ -47,5 +48,22 @@ extension Post {
                     mentions: self.mentions,
                     root: self.root,
                     text: textWithImages)
+    }
+}
+
+fileprivate let PlanetaryCompatBlobsGallery = "\n~=~=~Planetary Attachments~=~=~\n"
+
+extension String {
+    func withoutGallery() -> String {
+        guard let r1 = self.range(of: PlanetaryCompatBlobsGallery) else {
+            return self
+        }
+        let theRest = self[r1.upperBound..<self.endIndex]
+        
+        if !theRest.contains(PlanetaryCompatBlobsGallery) {
+            return self
+        }
+        let before = self[self.startIndex..<r1.lowerBound]
+        return String(before)
     }
 }
