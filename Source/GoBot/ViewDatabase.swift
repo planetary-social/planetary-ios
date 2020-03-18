f6d577e6ff896ab7cfa2ba31777fb429c39a16dd
Henry <cryptix@riseup.net>
2020-03-18 19:40:21 +0100

    sort view with most recent on top

--- Source/GoBot/ViewDatabase.swift
+++ Source/GoBot/ViewDatabase.swift
@@ -1111,6 +1111,7 @@ class ViewDatabase {
             .join(.leftOuter, self.tangles, on: self.tangles[colMessageRef] == self.channelAssigned[colMessageRef])
             .join(.leftOuter, self.posts, on: self.posts[colMessageRef] == self.channelAssigned[colMessageRef])
             .join(.leftOuter, self.votes, on: self.votes[colMessageRef] == self.channelAssigned[colMessageRef])
+            .order(colClaimedAt.desc)
 
         return try self.mapQueryToKeyValue(qry: qry)
     }
