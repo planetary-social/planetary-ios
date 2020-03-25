f1f9271b8ef1649c15fe269f6f6483b33c3931dd
Martin Dutra <martindsq@hotmail.com>
2020-03-24 23:31:54 -0300

    Fix mentions' avatar are wrongly displayed (#34)
    Problem: When typing a mention @something sometimes an avatar is not
    correct because the cell doesn't prepare for reuse.
    
    Solution: Reset the view when prepareForReuse is called.

--- Source/UI/AboutsMenu.swift
+++ Source/UI/AboutsMenu.swift
@@ -131,4 +131,9 @@ private class MiniAboutTableViewCell: UITableViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        aboutView.reset()
+    }
 }
