b46c6622409a4c126ff3e5607aa7dd121fd238f8
Henry <cryptix@riseup.net>
2020-04-08 11:38:13 +0200

    update go-ssb

--- go-ssb-bindings/vendor/go.cryptoscope.co/ssb/repo/badger_ios.go
+++ go-ssb-bindings/vendor/go.cryptoscope.co/ssb/repo/badger_ios.go
@@ -12,7 +12,7 @@ import (
 
 func badgerOpts(dbPath string) badger.Options {
 	opts := badger.DefaultOptions(dbPath)
-
+	opts.Logger = nil
 	// runtime throws MMIO can't allocate errors without this
 	// => badger failed to open: Invalid ValueLogLoadingMode, must be FileIO or MemoryMap
 	opts.ValueLogLoadingMode = options.FileIO
