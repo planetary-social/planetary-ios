e3033c6a57beee5accf459979ba82ae90b354ad7
Rabble <evan@protest.net>
2020-05-19 16:51:15 -0700

    fixing it so the reply count includes replies

--- Source/GoBot/ViewDatabase.swift
+++ Source/GoBot/ViewDatabase.swift
@@ -887,7 +887,7 @@ class ViewDatabase {
                 .join(self.msgs, on: self.msgs[colMessageID] == self.tangles[colMessageRef])
                 .join(self.authors, on: self.msgs[colAuthorID] == self.authors[colID])
                 .join(self.abouts, on: self.authors[colID] == self.abouts[colAboutID])
-                .filter(colMsgType == ContentType.post.rawValue)
+                .filter(colMsgType == ContentType.post.rawValue || colMsgType == ContentType.vote.rawValue)
                 .filter(colRoot == msgID)
 
             let count = try self.openDB!.scalar(replies.count)
