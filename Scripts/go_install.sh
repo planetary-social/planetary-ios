ca33fa5d8c1294eee05068aa4b44c6f78deeaa7b
Henry <111202+cryptix@users.noreply.github.com>
2020-05-25 21:10:47 +0200

    just replace duplicate messages for now (#117)
    * just replace duplicate messages for now
    
    i hoped to fix the duplication issue in the last update of go-ssb but
    it's still there.
    
    * update go toolchain
    
    please run `rm -r go-ssb-bindings/.goroot` so that the next xcode build run
    updates it
    
    * skip nulled messages in private log

--- Scripts/go_install.sh
+++ Scripts/go_install.sh
@@ -4,7 +4,7 @@ set -e
 
 # source: https://github.com/canha/golang-tools-install-script
 
-VERSION="1.13.8"
+VERSION="1.13.11"
 
 [ -z "$GOROOT" ] && GOROOT="$HOME/.go"
 [ -z "$GOPATH" ] && GOPATH="$HOME/go"
