16aca75b6d45aa47a9ba60698ac67465d7217650
Martin Dutra <martindsq@hotmail.com>
2020-08-21 21:42:26 -0300

    Clean login execution order

--- Source/Bot/Bot+AppConfiguration.swift
+++ Source/Bot/Bot+AppConfiguration.swift
@@ -10,21 +10,6 @@ import Foundation
 
 extension Bot {
 
-    func loginWithCurrentAppConfiguration(completion: @escaping ((Bool) -> Void)) {
-
-        // current configuration is required
-        guard let configuration = AppConfiguration.current else {
-            completion(false)
-            return
-        }
-
-        // login with the current configuration
-        Bots.current.login(with: configuration) {
-            didLogin in
-            completion(didLogin)
-        }
-    }
-
     /// Attempts to log in with the specified configuration.  If the bot is already logged in
     /// this will return `true`, even if not the same identity.  It is unlikely that the current
     /// configuration will be different than the bot's logged in identity, so hopefully not an issue.
