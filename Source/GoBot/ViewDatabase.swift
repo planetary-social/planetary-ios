ede1c512311f15d4b74bcff62b213c8442fc0826
Rabble <rabble@planetary.social>
2021-04-24 12:52:41 -0300

    tweaks

--- Source/GoBot/ViewDatabase.swift
+++ Source/GoBot/ViewDatabase.swift
@@ -877,7 +877,7 @@ class ViewDatabase {
     
     // who is this feed following?
     func getFollows(feed: Identity) throws -> [Identity] {
-f        guard let db = self.openDB else {
+       guard let db = self.openDB else {
             throw ViewDatabaseError.notOpen
         }
         
