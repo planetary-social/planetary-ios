3f23435ba0c885dd405785850c30d3656759dbdb
Matthew Lorentz <matt@lorentz.is>
2022-02-07 13:53:21 -0500

    Deleted testing.go, set ValueLogFileSize option

--- GoSSB/Sources/vendor/go.cryptoscope.co/ssb/repo/badger_ios.go
+++ GoSSB/Sources/vendor/go.cryptoscope.co/ssb/repo/badger_ios.go
@@ -13,6 +13,7 @@ import (
 
 func badgerOpts(dbPath string) badger.Options {
 	opts := badger.DefaultOptions(dbPath)
+	opts.ValueLogFileSize = 1 << 21
 	opts.Logger = nil
 	// runtime throws MMIO can't allocate errors without this
 	// => badger failed to open: Invalid ValueLogLoadingMode, must be FileIO or MemoryMap
