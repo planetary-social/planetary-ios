d284c40d97a4f34e33e4a419f8d1303f0953efc4
mplorentz <mplorentz@users.noreply.github.com>
2022-02-17 17:37:00 -0500

    Fix crash when tapping share button on Profile page (#378)

--- Source/Controller/AboutViewController.swift
+++ Source/Controller/AboutViewController.swift
@@ -199,10 +199,8 @@ class AboutViewController: ContentViewController {
 
             let activityController = UIActivityViewController(activityItems: [self.identity],
                                                               applicationActivities: nil)
+            activityController.configurePopover(from: sender)
             self.present(activityController, animated: true)
-            if let popOver = activityController.popoverPresentationController {
-                popOver.barButtonItem = self.navigationItem.rightBarButtonItem
-            }
         }
         actions.append(sharePublicIdentifier)
 
@@ -216,10 +214,8 @@ class AboutViewController: ContentViewController {
                 
                 let activityController = UIActivityViewController(activityItems: [text],
                                                                   applicationActivities: nil)
+                activityController.configurePopover(from: sender)
                 self.present(activityController, animated: true)
-                if let popOver = activityController.popoverPresentationController {
-                    popOver.barButtonItem = self.navigationItem.rightBarButtonItem
-                }
             }
             actions.append(share)
         }
@@ -331,8 +327,8 @@ class AboutViewController: ContentViewController {
 
             let activityController = UIActivityViewController(activityItems: [self.identity],
                                                               applicationActivities: nil)
+            activityController.configurePopover(from: sender)
             self.present(activityController, animated: true)
-            activityController.popoverPresentationController?.presentingViewController.configurePopover(from: sender)
         }
         actions.append(sharePublicIdentifier)
 
@@ -346,10 +342,8 @@ class AboutViewController: ContentViewController {
 
                 let activityController = UIActivityViewController(activityItems: [text],
                                                                   applicationActivities: nil)
+                activityController.configurePopover(from: sender)
                 self.present(activityController, animated: true)
-                activityController.popoverPresentationController?.presentingViewController.configurePopover(
-                    from: sender
-                )
             }
             actions.append(sharePublicLink)
         }
