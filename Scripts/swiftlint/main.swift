f3312b8d4274300b83ed63db546a9a9f9b61a1be
Matthew Lorentz <matt@lorentz.is>
2022-03-25 07:59:27 -0400

    Add a build target to run swiftlint in Xcode

---
+++ Scripts/swiftlint/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftlint
+//
+//  Created by Matthew Lorentz on 3/25/22.
+//  Copyright © 2022 Verse Communications Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
