e3605f95424e0e82b53032473f5bcfa2d2c1c5c1
Matthew Lorentz <matt@lorentz.is>
2022-04-21 18:46:03 -0400

    Remove code to run migration every time

--- Source/Controller/Beta1MigrationCoordinator.swift
+++ Source/Controller/Beta1MigrationCoordinator.swift
@@ -65,7 +65,7 @@ class Beta1MigrationCoordinator: ObservableObject, Beta1MigrationViewModel {
         }
         
         let version = userDefaults.string(forKey: GoBot.versionKey)
-        guard true || version == nil else {
+        guard version == nil else {
             return false
         }
         
