c70d45b7bd4cd5cf2dc49c9975031f16b3267f6d
Matthew Lorentz <matt@lorentz.is>
2022-06-22 14:28:59 -0400

    Change copy and tweak design on first two onboarding screens

--- Shared/Extensions/NSMutableAttributedString+Attributes.swift
+++ Shared/Extensions/NSMutableAttributedString+Attributes.swift
@@ -20,18 +20,27 @@ extension NSMutableAttributedString {
     /// Convenience func to add a `NSAttributedString.Key.link` value to a substring.
     func addLinkAttribute(value: String, to substring: String) {
         let range = (self.string as NSString).range(of: substring)
-        self.addAttributes([NSAttributedString.Key.link: value],
-                           range: range)
+        self.addAttributes([NSAttributedString.Key.link: value], range: range)
     }
 
     /// Convenience func to add a `NSAttributedString.Key.Font` and/or
     /// `NSAttributedString.Key.ForegroundColor` across the entire string.
     func addFontAttribute(_ font: UIFont, colorAttribute color: UIColor = .black) {
-        self.addAttributes([NSAttributedString.Key.font: font,
-                            NSAttributedString.Key.foregroundColor: color])
+        self.addAttributes([NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: color])
     }
 
     func addColorAttribute(_ color: UIColor) {
         self.addAttributes([NSAttributedString.Key.foregroundColor: color])
+    }
+    
+    func addParagraphAlignLeft() {
+        let style = NSMutableParagraphStyle()
+        style.alignment = .left
+        self.addAttributes([NSAttributedString.Key.paragraphStyle: style])
+    }
+    
+    func addAttributes(_ attributes: [NSAttributedString.Key: Any], to substring: String) {
+        let range = (self.string as NSString).range(of: substring)
+        self.addAttributes(attributes, range: range)
     }
 }
