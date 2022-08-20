7764e7c1b60305e257d85a305417d574b1c90651
Martin Dutra <martindsq@hotmail.com>
2022-08-19 20:59:16 -0700

    fix unittests

--- Source/GoBot/ViewDatabase.swift
+++ Source/GoBot/ViewDatabase.swift
@@ -454,26 +454,6 @@ class ViewDatabase {
         }
         return try strategy.countNumberOfKeys(connection: connection, userId: currentUserID)
     }
-
-    // posts for a feed
-    func stats(for feed: FeedIdentifier) throws -> Int {
-        guard let db = self.openDB else {
-            throw ViewDatabaseError.notOpen
-        }
-        do {
-            let authorID = try self.authorID(of: feed, make: false)
-            let theirRootPosts = try db.scalar(self.posts
-                .join(self.msgs, on: self.msgs[colMessageID] == self.posts[colMessageRef])
-                .filter(colAuthorID == authorID)
-                .filter(colIsRoot == true)
-                .count)
-
-            return theirRootPosts
-        } catch {
-            Log.optional(GoBotError.duringProcessing("stats for feed failed", error))
-            return 0
-        }
-    }
     
     func lastReceivedTimestamp() throws -> Double {
         guard let db = self.openDB else {
