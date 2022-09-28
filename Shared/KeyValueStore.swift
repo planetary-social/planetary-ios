241fba8aff85368ec260e966e7f9d0473ed4b4f0
Matthew Lorentz <matt@lorentz.is>
2022-09-28 11:59:28 -0400

    Mark UserDefaults as Sendable

---
+++ Shared/KeyValueStore.swift
@@ -0,0 +1,9 @@
+//
+//  KeyValueStore.swift
+//  Planetary
+//
+//  Created by Matthew Lorentz on 9/28/22.
+//  Copyright © 2022 Verse Communications Inc. All rights reserved.
+//
+
+import Foundation
