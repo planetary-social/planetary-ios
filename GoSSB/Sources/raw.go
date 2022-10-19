b366d7a807f6829a6c7c7ad4f9d845c26273a229
boreq <boreq@0x46.net>
2022-10-19 11:36:05 +0200

    Raw msg

--- GoSSB/Sources/raw.go
+++ GoSSB/Sources/raw.go
@@ -1,75 +1,49 @@
 package main
 
 import "C"
+import (
+	"github.com/pkg/errors"
+	"github.com/planetary-social/scuttlego/service/app/queries"
+	"github.com/planetary-social/scuttlego/service/domain/feeds/message"
+	"github.com/planetary-social/scuttlego/service/domain/refs"
+)
 
 // ssbGetRawMessage returns a raw message JSON for the specified feed and sequence number. Feed is in the @-notation,
 // sequence starts at 1.
 //export ssbGetRawMessage
 func ssbGetRawMessage(feedRef string, seq int64) *C.char {
-	return nil
-	//defer logPanic()
-	//
-	//var retErr error
-	//defer func() {
-	//	if retErr != nil {
-	//		level.Error(log).Log("where", "ssbGetRawMessage", "err", retErr)
-	//	}
-	//}()
-	//
-	//lock.Lock()
-	//defer lock.Unlock()
-	//if sbot == nil {
-	//	retErr = ErrNotInitialized
-	//	return nil
-	//}
-	//
-	//ref, err := refs.ParseFeedRef(feedRef)
-	//if err != nil {
-	//	retErr = errors.Wrap(err, "error parsing feed ref")
-	//	return nil
-	//}
-	//
-	//addr, err := feedStoredAddr(ref)
-	//if err != nil {
-	//	retErr = errors.Wrap(err, "failed to get the address used for storage")
-	//	return nil
-	//}
-	//
-	//uf, ok := sbot.GetMultiLog(multilogs.IndexNameFeeds)
-	//if !ok {
-	//	retErr = errors.New("failed to get the multilog")
-	//	return nil
-	//}
-	//
-	//log, err := uf.Get(addr)
-	//if err != nil {
-	//	retErr = errors.Wrap(err, "could not get the log")
-	//	return nil
-	//}
-	//
-	//sequenceNumberInReceiveLog, err := log.Get(seq - 1)
-	//if err != nil {
-	//	retErr = errors.Wrap(err, "could not get the message")
-	//	return nil
-	//}
-	//
-	//sequenceNumberInReceiveLogInt, ok := sequenceNumberInReceiveLog.(int64)
-	//if !ok {
-	//	retErr = errors.New("sequence number type is invalid")
-	//	return nil
-	//}
-	//
-	//msgInterface, err := sbot.ReceiveLog.Get(sequenceNumberInReceiveLogInt)
-	//if err != nil {
-	//	retErr = errors.Wrap(err, "could not get message from receive log")
-	//	return nil
-	//}
-	//
-	//msg, ok := msgInterface.(refs.Message)
-	//if !ok {
-	//	retErr = errors.New("message has invalid type")
-	//	return nil
-	//}
-	//
-	//return C.CString(string(msg.ValueContentJSON()))
+	var err error
+	defer logError("ssbGetRawMessage", &err)
+
+	service, err := node.Get()
+	if err != nil {
+		err = errors.Wrap(err, "could not get the node")
+		return nil
+	}
+
+	feed, err := refs.NewFeed(feedRef)
+	if err != nil {
+		err = errors.Wrap(err, "error creating a feed ref")
+		return nil
+	}
+
+	sequence, err := message.NewSequence(int(seq))
+	if err != nil {
+		err = errors.Wrap(err, "error creating a sequence")
+		return nil
+	}
+
+	query, err := queries.NewGetMessageBySequence(feed, sequence)
+	if err != nil {
+		err = errors.Wrap(err, "error creating the query")
+		return nil
+	}
+
+	msg, err := service.App.Queries.GetMessageBySequence.Handle(query)
+	if err != nil {
+		err = errors.Wrap(err, "error calling the handler")
+		return nil
+	}
+
+	return C.CString(string(msg.Raw().Bytes()))
 }
