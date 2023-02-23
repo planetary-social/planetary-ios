d2367c42582930e853c878df27bbe456fba0e73d
boreq <boreq@0x46.net>
2023-02-23 18:03:42 +0100

    Revert "Bump Go from 1.19 to 1.20 (#1196)"
    This reverts commit dd3146d46d58b321c37fcc9cc42864d1d80746cf.

--- Scripts/go_install.sh
+++ Scripts/go_install.sh
@@ -4,7 +4,7 @@ set -e
 
 # source: https://github.com/canha/golang-tools-install-script
 
-VERSION="1.20"
+VERSION="1.19"
 
 [ -z "$GOROOT" ] && GOROOT="$HOME/.go"
 [ -z "$GOPATH" ] && GOPATH="$HOME/go"
